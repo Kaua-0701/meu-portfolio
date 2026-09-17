ca31428a7423e3037390b0e90eabb7a8dcc2479e
Kauã Ribeiro Mendes <ribeirocyber123@gmail.com>
2026-09-16 21:28:11 -0300

    fix: "logos das redes sociais ajustadas"

--- src/js/index.js
+++ src/js/index.js
@@ -17,17 +17,21 @@
 const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
 const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)');
 
-botaoMostrarProjetos.addEventListener('click', () => {
-    // Passo 3 - adicionar a classe "ativo" nos projetos escondidos
-    mostrarMaisProjetos();
+if (botaoMostrarProjetos) {
+    botaoMostrarProjetos.addEventListener('click', () => {
+        // Passo 3 - adicionar a classe "ativo" nos projetos escondidos
+        mostrarMaisProjetos();
 
-    // Objetivo 2 - esconder o botão de mostrar mais
-    // Passo 1 - pegar o botão e esconder ele
-    esconderBotao();
-});
+        // Objetivo 2 - esconder o botão de mostrar mais
+        // Passo 1 - pegar o botão e esconder ele
+        esconderBotao();
+    });
+}
 
 function esconderBotao() {
-    botaoMostrarProjetos.classList.add("remover");
+    if (botaoMostrarProjetos) {
+        botaoMostrarProjetos.classList.add("remover");
+    }
 }
 
 function mostrarMaisProjetos() {
